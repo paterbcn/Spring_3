d10d1b2d71224c7fd6cf178531e818fb4c3bbbf3
pater <agustinpaternoster@gmail.com>
2023-03-31 12:23:21 +0200

    start

---
+++ Entrega3.1_N1.js
@@ -0,0 +1,3 @@
+//El código adjunto lee un fichero situado en un directorio inbox y escribe su 
+//contenido invertido en otro fichero en el directorio outbox. 
+//Reestructura y simpldique el código existente para evitar el denominado Callback Hell.
